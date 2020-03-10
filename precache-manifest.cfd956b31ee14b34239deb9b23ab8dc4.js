fae8908948c8e0eaf4cc31626e640f45db73b0af
tymkowy <tymekjw@gmail.com>
2020-03-10 10:33:16 +0100

    Updates

---
+++ precache-manifest.cfd956b31ee14b34239deb9b23ab8dc4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d255bc634040b2e51df076ceffeb40cf",
+    "url": "/coronavirus-stats/index.html"
+  },
+  {
+    "revision": "14259d36759b2c472ef6",
+    "url": "/coronavirus-stats/static/css/2.2a59c9fe.chunk.css"
+  },
+  {
+    "revision": "979e2d47a15844fa353d",
+    "url": "/coronavirus-stats/static/css/main.fb085eba.chunk.css"
+  },
+  {
+    "revision": "14259d36759b2c472ef6",
+    "url": "/coronavirus-stats/static/js/2.696e3e83.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coronavirus-stats/static/js/2.696e3e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979e2d47a15844fa353d",
+    "url": "/coronavirus-stats/static/js/main.4138eaeb.chunk.js"
+  },
+  {
+    "revision": "7471f1713e983e603a15",
+    "url": "/coronavirus-stats/static/js/runtime-main.d793672d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coronavirus-stats/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coronavirus-stats/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coronavirus-stats/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coronavirus-stats/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coronavirus-stats/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6f9a4fcef5b2768e90251a4751afb1e3",
+    "url": "/coronavirus-stats/static/media/virus.6f9a4fce.svg"
+  }
+]);
